e3c99ab7b265bb81258d7966b1ae7c569756084a
hieu26 <dtc225190011@ictu.edu.vn>
2025-02-14 22:30:08 +0700

    ..

---
+++ Delegate_HW2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Delegate_HW2
+{
+    internal class Program
+    {
+        delegate int ComparisonDelegate<T>(T x, T y);
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Name = "John", Age = 30, Salary = 60000 },
+                new Employee { Name = "Jane", Age = 25, Salary = 65000 },
+                new Employee { Name = "Doe", Age = 35, Salary = 55000 }
+            };
+
+            Sort(employees, CompareByName);
+            Console.WriteLine("Sorted by Name:");
+            employees.ForEach(e => Console.WriteLine(e));
+
+            Sort(employees, CompareByAge);
+            Console.WriteLine("\nSorted by Age:");
+            employees.ForEach(e => Console.WriteLine(e));
+
+            Sort(employees, CompareBySalary);
+            Console.WriteLine("\nSorted by Salary:");
+            employees.ForEach(e => Console.WriteLine(e));
+        }
+
+        static void Sort<T>(List<T> list, ComparisonDelegate<T> comparison)
+        {
+            list.Sort((x, y) => comparison(x, y));
+        }
+
+        static int CompareByName(Employee x, Employee y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+
+        static int CompareByAge(Employee x, Employee y)
+        {
+            return x.Age.CompareTo(y.Age);
+        }
+
+        static int CompareBySalary(Employee x, Employee y)
+        {
+            return x.Salary.CompareTo(y.Salary);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Salary: {Salary}";
+        }
+    }
+}
---
+++ Delegate_HW3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Delegate_HW3
+{
+    internal class Program
+    {
+        delegate bool Predicate(int x);
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Odd numbers:");
+            Filter(array, x => x % 2 != 0);
+            Console.WriteLine("Even numbers:");
+            Filter(array, x => x % 2 == 0);
+            Console.WriteLine("Numbers greater than 5:");
+            Filter(array, x => x > 5);
+        }
+        static void Filter(int[] array, Predicate predicate)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (predicate(array[i]))
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
